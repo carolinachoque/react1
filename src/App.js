e45ebf2685fb426de9242b62354a19a326b6b35e
carolina298 <roussemodas298@gmail.com>
2023-11-11 23:04:53 -0500

    practica de componentes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import C01componente from './componentes/C01componente';
 import C02contador from './componentes/C02contador';
 import Variables from './componentes/C04variables';
+import C06matriz from './componentes/C06matriz';
 
 function App() {
   return (
-    <div>
-    < C01componente />
-    < C02contador />
-    < Variables xVariable="Hola mundo" />
-    </div>
+   <div>
+     < C01componente />
+     < C02contador />
+     < Variables xVariable="Hola mundo" />
+     <C06matriz />
+   </div>
   );
 }
 
--- src/componentes/C06matriz.js
+++ src/componentes/C06matriz.js
@@ -1,15 +1,15 @@
-/*import React {useState, Fragment} from "react";
+import React, { Fragment, useState } from 'react'
 
-const C06matriz = () => { 
-    const [lista, setLista] = useState ([1,2,3,4,5]);
-    return (
-        <Fragment>
-            <h1 style={{background:"green"}} >lectura sin index y con html</h1>
-            {lista.map((item) => <span> {item}</span>)}
-            <br/>
-            
-        </Fragment>   
-    );
+const C06matriz = () => {
+    const [lista, setLista] = useState([1,2,3,4,5]);
+  return (
+    <Fragment>
+        <h1 style={{background:"green"}}>lectura sin index y con html</h1>
+        {lista.map((item) => <span> {item} </span>)}
+        <br/>
+      
+    </Fragment>
+  );
 }
+
 export default C06matriz
-*/
