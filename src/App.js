60fb07015c9570f206efcd0fb5846858ad9803a3
carolina298 <roussemodas298@gmail.com>
2023-11-11 19:30:27 -0500

    practica de componentes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 
 import logo from './logo.svg';
 import './App.css';
+import C01componente from './componentes/C01componente';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < C01componente />
     </div>
   );
 }
 
 export default App;
-import logo from './logo.svg';
-import './App.css';
-import C01componente from './componentes/C01componente';
-import C02contador from './componentes/C02contador';
-import Variables from './componentes/C04variables';
-import C06matriz from './componentes/C06matriz';
-
-function App() {
-  return (
-    <div>
-      <C01componente />
-      
-    </div>
-  );
-}
-
-
